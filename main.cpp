997901159b004a170472629a1b13669cc50f31d5
ameley <amelleymur@gmail.com>
2019-05-14 17:36:07 +0400

    Add Subtraction

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	int a,b,x;
+	int a,b,x,y;
 	cout<<"Enter number a:\n";
 	cin>>a;
 	cout<<"Enter number b:\n";
 	cin>>b;
 	x=a+b;
-	cout<<"a+b=\n"<<x;
+	cout<<"a+b="<<x<<"\n";
+	y=a-b;
+	cout<<"a-b=\n"<<y<<"\n";
 	return 0;
 }
